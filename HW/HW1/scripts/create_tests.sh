ab8f7ae83186c56612b095ee538f43bbb5c2534a
Sayemum Hassan <sayemumhassan@gmail.com>
2024-08-30 17:34:59 -0400

    Shell split safety test

--- HW/HW1/scripts/create_tests.sh
+++ HW/HW1/scripts/create_tests.sh
@@ -13,13 +13,37 @@ OUTPUT_PATH="output"
 # TOTAL_SIZE=$(wc -c < $INPUT_PATH)
 # wc -c < $INPUT_PATH
 
-INPUT_200K=$(head -c 204800 $INPUT_PATH > chunk_200K.txt)
-INPUT_400K=$(head -c 409600 $INPUT_PATH > chunk_400K.txt)
-INPUT_600K=$(head -c 614400 $INPUT_PATH > chunk_600K.txt)
-INPUT_800K=$(head -c 819200 $INPUT_PATH > chunk_800K.txt)
+# INPUT_200K=$(head -c 204800 $INPUT_PATH > chunk_200K.txt)
+# INPUT_400K=$(head -c 409600 $INPUT_PATH > chunk_400K.txt)
+# INPUT_600K=$(head -c 614400 $INPUT_PATH > chunk_600K.txt)
+# INPUT_800K=$(head -c 819200 $INPUT_PATH > chunk_800K.txt)
+# head -c 204800 $INPUT_PATH > chunk_200K.txt
+# head -c 409600 $INPUT_PATH > chunk_400K.txt
+# head -c 614400 $INPUT_PATH > chunk_600K.txt
+# head -c 819200 $INPUT_PATH > chunk_800K.txt
+split -b 200K $INPUT_PATH "${OUTPUT_PATH}/chunk_200K_"
 
 # Test file sizes with wc
+echo "Size of chunk_200K.txt:"
 wc -c < chunk_200K.txt
-wc -c < chunk_400K.txt
-wc -c < chunk_600K.txt
-wc -c < chunk_800K.txt
+# echo "Size of chunk_400K.txt:"
+# wc -c < chunk_400K.txt
+# echo "Size of chunk_600K.txt:"
+# wc -c < chunk_600K.txt
+# echo "Size of chunk_800K.txt:"
+# wc -c < chunk_800K.txt
+
+
+# Run the dict version using time (with small input)
+# (time python "../cs412_foxsays_dict.py" < "../foxsays_t1_in.txt")
+# With the bigger inputs:
+# (time python "../cs412_foxsays_dict.py" < "chunk_200K.txt")
+
+
+
+# Run line_plot.py to create the plots
+# Create the plots directory if it doesn't exist
+# PLOTS_DIR="plots"
+# mkdir -p $PLOTS_DIR
+
+# python "../line_plot.py"
