5319be3def491082d65188f7b7332817cf23ca07
= <sayemumhassan@gmail.com>
2024-10-06 11:18:14 -0400

    Lab 9 Progress

--- Labs/Lab9/cs412_knapsack_fractional.py
+++ Labs/Lab9/cs412_knapsack_fractional.py
@@ -9,7 +9,7 @@
 """
 
 
-def knapsack(i, w):
+def frac_knapsack(i, w):
     pass
 
 
@@ -20,15 +20,37 @@ def main():
     # section_lengths = list(map(int, input().split()))
     # height = int(input())
     
-    weight = int(input())
+    w = int(input())
     n = int(input())
     
     triples = []
     for _ in range(n):
         line = input().split()
-        triples.append((line[0], float(line[1]), float(line[2])))
+        # triples.append((line[0], float(line[1]), float(line[2])))
+        triples.append([line[0], float(line[1]), float(line[2])])
 
-    print(triples)
+    # print(triples)
+    
+    triples.sort(key=lambda x: (x[1] / x[2]), reverse=True)
+    
+    # print(triples)
+    
+    total = 0.0
+    
+    for item in triples:
+        if item[2] <= w:
+            w -= item[2]
+            total += item[1]
+        else:
+            total += item[1] * w / item[2]
+            break
+    
+    # print(triples)
+    for item in triples:
+        print(f"{item[0]}({item[1]:.2f}, {item[2]:.2f})", end=" ")
+    
+    print()
+    print(total)
 
 if __name__ == "__main__":
     main()
