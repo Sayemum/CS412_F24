6e6498b2ba432d64ae71a20b0f9312e8bdd96e3f
= <sayemumhassan@gmail.com>
2024-10-07 23:36:24 -0400

    Lab 10 Init

---
+++ Labs/Lab10/cs412_bus_stops_a.py
@@ -0,0 +1,25 @@
+"""
+    name:  Sayemum Hassan
+
+    Honor Code and Acknowledgments:
+
+            This work complies with the JMU Honor Code.
+
+           Comments here on your code and submission.
+"""
+
+
+def main():
+    # get input
+    
+    n = int(input())
+    
+    stops = {}
+    edges = set([])
+    for _ in range(n):
+        line = input().split()
+        triples.append([line[0], float(line[1]), float(line[2])])
+    
+
+if __name__ == "__main__":
+    main()
