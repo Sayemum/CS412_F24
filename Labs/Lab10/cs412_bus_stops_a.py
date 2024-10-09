0f5afb4d5e9c19c9496f1154cb5c156161a45147
Sayemum Hassan <sayemumhassan@gmail.com>
2024-10-09 12:44:45 -0400

    Lab 10 Progress

--- Labs/Lab10/cs412_bus_stops_a.py
+++ Labs/Lab10/cs412_bus_stops_a.py
@@ -1,5 +1,5 @@
 """
-    name:  Sayemum Hassan
+    name: Sayemum Hassan
 
     Honor Code and Acknowledgments:
 
@@ -9,16 +9,61 @@
 """
 
 
+def search(stack, stops_connected, startV, endV):
+    stack.append(startV)
+    stops = list(stops_connected.keys())
+    correct_path = []
+    # print(type(stops))
+    # print(stops)
+    
+    while stack:
+        v = stack.pop()
+        correct_path = [v]
+        
+        if v in stops:
+            stops.remove(v)
+            
+            for w in stops_connected[v]:
+                stack.append(w)
+                correct_path.append(w)
+
+    return correct_path
+
+# USE AN AJASCENCY LIST
 def main():
     # get input
     
-    n = int(input())
+    # n = int(input())
     
-    stops = {}
-    edges = set([])
-    for _ in range(n):
-        line = input().split()
-        triples.append([line[0], float(line[1]), float(line[2])])
+    # Key is each stop as string and value is a list of all connected stops
+    # stops_connected = {}
+    # for _ in range(n):
+    #     line = input().split()
+    #     stop1, stop2 = line[0], line[1]
+        
+    #     if stops_connected.get(stop1) == None:
+    #         stops_connected[stop1] = []
+    #         stops_connected[stop1].append(stop2)
+    #     else:
+    #         stops_connected[stop1].append(stop2)
+        
+    #     if stops_connected.get(stop2) == None:
+    #         stops_connected[stop2] = []
+    #         stops_connected[stop2].append(stop1)
+    #     else:
+    #         stops_connected[stop2].append(stop1)
+    
+    stops_connected = {'Hburg': ['JMU'], 'JMU': ['Hburg', 'Charlottesville'], 'Charlottesville': ['JMU']}
+    
+    # query = input().split()
+    query = ['Hburg', 'Charlottesville']
+    stack = []
+    correct_path = search(stack, stops_connected, query[0], query[1])
+    
+    # print(stack)
+    print(" ".join(correct_path))
+    print()
+    print(stops_connected)
     
 
 if __name__ == "__main__":
