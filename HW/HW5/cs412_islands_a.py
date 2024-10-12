78b494c20aa1c0cc63ab48c7d55f725fbebe5790
= <sayemumhassan@gmail.com>
2024-10-12 11:40:09 -0400

    HW5 Init

---
+++ HW/HW5/cs412_islands_a.py
@@ -0,0 +1,38 @@
+"""
+    name: Sayemum Hassan
+
+    Honor Code and Acknowledgments:
+
+            This work complies with the JMU Honor Code.
+
+           Comments here on your code and submission.
+"""
+
+
+def iterative():
+    largest_count = 0
+    
+    
+    
+    return largest_count
+
+
+def main():
+    # get input
+    
+    # n = int(input())
+    
+    # region = []
+    # for _ in range(n):
+    #     line = input().split()
+        
+    #     row = [int(num) for num in line]
+    #     region.append(row)
+    
+    # print(region)
+    
+    region = [[0, 1], [1, 1]]
+    print(region[0][1])
+
+if __name__ == "__main__":
+    main()
