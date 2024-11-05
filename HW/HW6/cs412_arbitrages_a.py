04df75e7be675345cdbdaa63aa3cb2e0268cfc53
= <sayemumhassan@gmail.com>
2024-11-04 22:14:41 -0500

    HW 6 Progress

--- HW/HW6/cs412_arbitrages_a.py
+++ HW/HW6/cs412_arbitrages_a.py
@@ -11,10 +11,6 @@
 import math
 
 
-def arbitrage(num_rates, exchanges):
-    pass
-    # detect if product of all exchanges is greater than 1
-
 def create_graph(exchanges):
     graph = []
     
@@ -23,12 +19,16 @@ def create_graph(exchanges):
     
     return graph
 
-def InitSSSP(verticesNum):
-    graph = []
+def InitSSSP(given):
+    graph = {}
     
-    for i in range(verticesNum):
-        graph.append([i, float('inf'), None])
-    graph[0][1] = 0
+    for u in given:
+        graph[u] = {float('inf') : None}
+    graph[0] = 0
+    
+    # for i in range(verticesNum):
+    #     graph.append([i, float('inf'), None])
+    # graph[0][1] = 0
     
     return graph
 
@@ -36,29 +36,36 @@ def Bellman_Ford(num_rates, exchanges):
     graph = InitSSSP(num_rates)
     
     # relax edges v - 1 times
-    for _ in range(num_rates - 1):
-        for exchange in exchanges:
-            print(exchange)
+    # for _ in range(num_rates - 1):
+    #     for exchange in exchanges:
+    #         print(exchange)
     
     return graph
+
+def detect_arbitrage(num_rates, exchanges):
+    graph = Bellman_Ford(num_rates, exchanges)
+    # detect if product of all exchanges is greater than 1
+    return False
 
 def main():
     # example input
     num_rates = 4
-    exchanges = [('USD', 'EUR', 0.82), ('EUR', 'JPY', 129.7), ('JPY', 'TRY', 12.0), ('TRY', 'USD', 0.0008)]
+    exchanges = {'USD': {'EUR': 0.82}, 'EUR': {'JPY': 129.7}, 'JPY': {'TRY': 12.0}, 'TRY': {'USD': 0.0008}}
     
     # get input
     # num_rates = int(input())
-    # exchanges = []
+    # exchanges = {}
     
     # for _ in range(num_rates):
     #     exchange = input().split()
-    #     exchanges.append((exchange[0], exchange[1], float(exchange[2])))
+    #     exchanges[exchange[0]] = {exchange[1] : float(exchange[2])}
     
     print(exchanges)
-    graph = create_graph(exchanges)
-    print(graph)
-    bellman = Bellman_Ford(num_rates, exchanges)
+    # graph = create_graph(exchanges)
+    # print(graph)
+    # bellman = Bellman_Ford(num_rates, exchanges)
+    detected = detect_arbitrage(num_rates, exchanges)
+    print(detected)
 
 if __name__ == "__main__":
     main()
